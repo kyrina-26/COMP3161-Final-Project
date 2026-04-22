6e6fb6f8a5d6d9f32c2bfe4d64f55cd91caf0e41
Daniel Bingham <danielbingham000@gmail.com>
2026-04-21 23:06:34 -0500

    feat: added the preliminary inserts file

---
+++ scripts/inserts.py
@@ -0,0 +1,11 @@
+import faker
+import random
+def insert():
+    print("Inserting data into the database...")
+    # Code to insert data into the database goes here
+    # create the most independent tabls first and then the dependent tables
+    #Independent tables: users, courses. 
+    # dependent tables: everything else. (easiest to fill is student, admin and instructor tables, then teaches will be next as it is more simple than enrollment, then enrollment)
+    pass
+if __name__ == "__main__":
+    insert()
