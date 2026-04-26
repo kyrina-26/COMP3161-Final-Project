fba4f7e02519889704eaf28842ef79c239576576
kyrina-26 <kyrina.mollison@utronto.ca>
2026-04-26 04:01:41 -0500

    feat: added sql files for data insertion

---
+++ database/03-administrators.sql
@@ -0,0 +1,11 @@
+-- Insert administrators
+INSERT IGNORE INTO administrators (first_name, last_name, user_id) VALUES ('Ashley', 'Diaz', 1);
+INSERT IGNORE INTO administrators (first_name, last_name, user_id) VALUES ('Deborah', 'Meyer', 2);
+INSERT IGNORE INTO administrators (first_name, last_name, user_id) VALUES ('William', 'Smith', 3);
+INSERT IGNORE INTO administrators (first_name, last_name, user_id) VALUES ('Megan', 'Chavez', 4);
+INSERT IGNORE INTO administrators (first_name, last_name, user_id) VALUES ('Richard', 'Santiago', 5);
+INSERT IGNORE INTO administrators (first_name, last_name, user_id) VALUES ('Amanda', 'Smith', 6);
+INSERT IGNORE INTO administrators (first_name, last_name, user_id) VALUES ('Eric', 'Smith', 7);
+INSERT IGNORE INTO administrators (first_name, last_name, user_id) VALUES ('Stephen', 'Martinez', 8);
+INSERT IGNORE INTO administrators (first_name, last_name, user_id) VALUES ('Gerald', 'Conway', 9);
+INSERT IGNORE INTO administrators (first_name, last_name, user_id) VALUES ('Bradley', 'Hill', 10);
